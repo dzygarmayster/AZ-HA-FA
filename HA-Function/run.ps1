8d9814e4cccb8668294e51e41c492213570bbd73
dzygarmayster <d.voytkov@gmail.com>
2024-04-08 10:32:52 +0300

    Update run.ps1

--- HA-Function/run.ps1
+++ HA-Function/run.ps1
@@ -18,11 +18,20 @@ Write-Output -InputObject "HA NVA timer trigger function executed at:$(Get-Date)
 # Set firewall monitoring variables here
 #--------------------------------------------------------------------------
 
-
-
 $VMFW1Name = $env:FW1NAME      # Set the Name of the primary NVA firewall
+$VMFW2Name = $env:FW2NAME      # Set the Name of the secondary NVA firewall
 $FW1RGName = $env:FWRGNAME     # Set the ResourceGroup that contains FW1
+$FW2RGName = $env:FWRGNAME     # Set the ResourceGroup that contains FW2
+$Monitor = $env:FWMONITOR      # "VMStatus" or "TCPPort" are valid values
 
+#--------------------------------------------------------------------------
+# Set the failover and failback behavior for the firewalls
+#--------------------------------------------------------------------------
+
+$FailOver = $True              # Trigger to enable fail-over to secondary NVA firewall if primary NVA firewall drops when active
+$FailBack = $True              # Trigger to enable fail-back to primary NVA firewall is secondary NVA firewall drops when active
+$IntTries = $env:FWTRIES       # Number of Firewall tests to try 
+$IntSleep = $env:FWDELAY       # Delay in seconds between tries
 
 
 #--------------------------------------------------------------------------
@@ -47,6 +56,11 @@ Function Test-VMStatus ($VM, $FWResourceGroup)
 
 
 
+#--------------------------------------------------------------------------
+# Main code block for Azure function app                       
+#--------------------------------------------------------------------------
+
+
 $Password = ConvertTo-SecureString $env:SP_PASSWORD -AsPlainText -Force
 $Credential = New-Object System.Management.Automation.PSCredential ($env:SP_USERNAME, $Password)
 $AzureEnv = Get-AzEnvironment -Name $env:AZURECLOUD
@@ -55,8 +69,10 @@ Connect-AzAccount -ServicePrincipal -Tenant $env:TENANTID -Credential $Credentia
 $Context = Get-AzContext
 Set-AzContext -Context $Context
 
-Write-Host $FW1RGName $VMFW1Name
-
 $FW1Down = Test-VMStatus -VM $VMFW1Name -FwResourceGroup $FW1RGName
 
 Write-Host " FWDown is: $FW1Down "
+
+$Res = Find-AzureRmResource -TagName nva_ha_udr -TagValue $TagValue
+
+Write-Host $Res
