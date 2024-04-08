6bd95ede33ea2a4e6e950cbf36bd4772b8fb837d
dzygarmayster <d.voytkov@gmail.com>
2024-04-08 13:16:08 +0300

    Update run.ps1

--- HA-Function/run.ps1
+++ HA-Function/run.ps1
@@ -55,7 +55,83 @@ Function Test-VMStatus ($VM, $FWResourceGroup)
   Return $True  
 }
 
+Function Start-Failover 
+{
+  foreach ($SubscriptionID in $Script:ListOfSubscriptionIDs){
+    Set-AzContext -Subscription $SubscriptionID
+    $RTable = @()
+    $TagValue = $env:FWUDRTAG
+    $Res = Get-AzResource -TagName nva_ha_udr -TagValue $TagValue
 
+    foreach ($RTable in $Res)
+    {
+      $Table = Get-AzRouteTable -ResourceGroupName $RTable.ResourceGroupName -Name $RTable.Name
+      
+      foreach ($RouteName in $Table.Routes)
+      {
+        Write-Output -InputObject "Updating route table..."
+        Write-Output -InputObject $RTable.Name
+
+        for ($i = 0; $i -lt $PrimaryInts.count; $i++)
+        {
+          if($RouteName.NextHopIpAddress -eq $SecondaryInts[$i])
+          {
+            Write-Output -InputObject 'Secondary NVA is already ACTIVE' 
+            
+          }
+          elseif($RouteName.NextHopIpAddress -eq $PrimaryInts[$i])
+          {
+            Set-AzRouteConfig -Name $RouteName.Name  -NextHopType VirtualAppliance -RouteTable $Table -AddressPrefix $RouteName.AddressPrefix -NextHopIpAddress $SecondaryInts[$i] 
+          }
+        }
+
+      }
+  
+      $UpdateTable = [scriptblock]{param($Table) Set-AzRouteTable -RouteTable $Table}
+      &$UpdateTable $Table
+
+    }
+  }
+
+#  Send-AlertMessage -message "NVA Alert: Failover to Secondary FW2"
+
+}
+
+Function Get-FWInterfaces
+{
+  $Nics = Get-AzNetworkInterface | Where-Object -Property VirtualMachine -NE -Value $Null
+  $VMS1 = Get-AzVM -Name $VMFW1Name -ResourceGroupName $FW1RGName
+  $VMS2 = Get-AzVM -Name $VMFW2Name -ResourceGroupName $FW2RGName
+
+  foreach($Nic in $Nics)
+  {
+
+    if (($Nic.VirtualMachine.Id -EQ $VMS1.Id) -Or ($Nic.VirtualMachine.Id -EQ $VMS2.Id)) 
+    {
+      $VM = $VMS | Where-Object -Property Id -EQ -Value $Nic.VirtualMachine.Id
+      $Prv = $Nic.IpConfigurations | Select-Object -ExpandProperty PrivateIpAddress  
+
+      if ($VM.Name -eq $VMFW1Name)
+      {
+        $Script:PrimaryInts += $Prv
+      }
+      elseif($VM.Name -eq $vmFW2Name)
+      {
+        $Script:SecondaryInts += $Prv
+      }
+
+    }
+
+  }
+}
+
+
+Function Get-Subscriptions
+{
+  Write-Output -InputObject "Enumerating all subscriptins ..."
+  $Script:ListOfSubscriptionIDs = (Get-AzSubscription).Subscription
+  Write-Output -InputObject $Script:ListOfSubscriptionIDs
+}
 
 #--------------------------------------------------------------------------
 # Main code block for Azure function app                       
@@ -69,6 +145,22 @@ Connect-AzAccount -ServicePrincipal -Tenant $env:TENANTID -Credential $Credentia
 
 $Context = Get-AzContext
 Set-AzContext -Context $Context
+
+$Script:PrimaryInts = @()
+$Script:SecondaryInts = @()
+$Script:ListOfSubscriptionIDs = @()
+
+# Check NVA firewall status $intTries with $intSleep between tries
+
+$CtrFW1 = 0
+$CtrFW2 = 0
+$FW1Down = $True
+$FW2Down = $True
+
+$VMS = Get-AzVM
+
+Get-Subscriptions
+Get-FWInterfaces
 
 $FW1Down = Test-VMStatus -VM $VMFW1Name -FwResourceGroup $FW1RGName
 
